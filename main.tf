a5a96cd17eac67349ad5884d90fb3f865e0e9fdc
Michal Stepanek <micstepanek@gmail.com>
2020-10-29 13:12:57 +0100

    Try resource dependencies

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("test-293616-36f12f57dcc0.json")
+
+  project = "test-293616"
+  region  = "europe-west3"
+  zone    = "europe-west3-b"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
